6bf9e5f70d1f64676dcd8beebd34156f60cc907a
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2021-08-18 01:06:55 +0200

    Added a UITableView to the MainViewController and configured the UITableViewDataSource methods.

--- Next-Stop/Controllers/MainViewController.swift
+++ Next-Stop/Controllers/MainViewController.swift
@@ -8,22 +8,52 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    // MARK: - UI Elements
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Properties
+    
+    var items: [Place] = []
+    
+    // MARK: - Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.setupTableView()
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    ///
+    /// Setup the TableView
+    ///
+    private func setupTableView() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
-    */
+    
+}
 
+// MARK: - UITableView Delegate
+
+extension MainViewController: UITableViewDelegate {}
+
+// MARK: - UITableView Data Source
+
+extension MainViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell") as! UITableViewCell
+        cell.textLabel?.text = "Prueba"
+        return cell
+    }
+    
 }
