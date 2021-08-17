e9a1b8481b600d2a07228b29400833df9fa902e1
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2021-08-17 21:33:05 +0200

    Added the object Place.

---
+++ Next-Stop/Models/Place.swift
@@ -0,0 +1,13 @@
+//
+//  Place.swift
+//  Next-Stop
+//
+//  Created by Aitor Zubizarreta Perez on 17/08/2021.
+//
+
+import Foundation
+
+struct Place: Codable {
+    var id: UUID
+    var name: String
+}
