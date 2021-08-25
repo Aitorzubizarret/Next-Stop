c86ba3ba6f4bc66d13fddb7c65ccf513de4e4d42
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2021-08-25 22:00:22 +0200

    Added 12 colours to Resources. Created the file Template and extended UIColor to add those 12 custom colours to it.

---
+++ Next-Stop/Template.swift
@@ -0,0 +1,25 @@
+//
+//  Template.swift
+//  Next-Stop
+//
+//  Created by Aitor Zubizarreta Perez on 25/08/2021.
+//
+
+import UIKit
+
+extension UIColor {
+    static let custom_blue_gray: UIColor = UIColor(named: "blue-gray")!
+    static let custom_khaki: UIColor = UIColor(named: "khaki")!
+    static let custom_mint_blue: UIColor = UIColor(named: "mint-blue")!
+    static let custom_salmon: UIColor = UIColor(named: "salmon")!
+    
+    static let custom_cinnabar: UIColor = UIColor(named: "cinnabar")!
+    static let custom_ebony: UIColor = UIColor(named: "ebony")!
+    static let custom_khaki_2: UIColor = UIColor(named: "khaki-2")!
+    static let custom_khaki_3: UIColor = UIColor(named: "khaki-3")!
+    
+    static let custom_misty_blue: UIColor = UIColor(named: "misty-blue")!
+    static let custom_orange: UIColor = UIColor(named: "orange")!
+    static let custom_orange_2: UIColor = UIColor(named: "orange-2")!
+    static let custom_peach: UIColor = UIColor(named: "peach")!
+}
