4c75b55b9c6aa71585305adfe955f595002c51a3
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2021-08-28 15:49:20 +0200

    Added a Tableview to NoteDetailViewController.

--- Next-Stop/Controllers/NoteDetailViewController.swift
+++ Next-Stop/Controllers/NoteDetailViewController.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 class NoteDetailViewController: UIViewController {
-
+    
+    // MARK: - UI Elements
+    
+    @IBOutlet weak var tableview: UITableView!
+    
+    // MARK: - Properties
+    
+    // MARK: - Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
