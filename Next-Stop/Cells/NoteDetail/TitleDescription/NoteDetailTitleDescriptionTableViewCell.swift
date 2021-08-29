47a9a7a2c4aeb4b1a10d48de7d79ade4b8d1185b
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2021-08-29 22:00:54 +0200

    Created the cell NoteDetailTitleDescriptionTableViewCell. Updated the note detail view controller. Now it has a method to setup the table view, and displays the new cell.

---
+++ Next-Stop/Cells/NoteDetail/TitleDescription/NoteDetailTitleDescriptionTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NoteDetailTitleDescriptionTableViewCell.swift
+//  Next-Stop
+//
+//  Created by Aitor Zubizarreta Perez on 29/08/2021.
+//
+
+import UIKit
+
+class NoteDetailTitleDescriptionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Next-Stop/Controllers/NoteDetailViewController.swift
+++ Next-Stop/Controllers/NoteDetailViewController.swift
@@ -15,12 +15,15 @@ class NoteDetailViewController: UIViewController {
     
     // MARK: - Properties
     
+    private let noteDetailTitleDescriptionCellName: String = "NoteDetailTitleDescriptionTableViewCell"
+    
     // MARK: - Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.setupView()
+        self.setupTableView()
     }
     
     ///
@@ -28,6 +31,44 @@ class NoteDetailViewController: UIViewController {
     ///
     private func setupView() {
         self.title = "Note Detail VC"
+    }
+    
+    ///
+    /// Setup the TableView.
+    ///
+    private func setupTableView() {
+        self.tableview.delegate = self
+        self.tableview.dataSource = self
+        
+        self.tableview.separatorStyle = .none
+        
+        // Register the cells.
+        let titleDescriptionCell: UINib = UINib(nibName: self.noteDetailTitleDescriptionCellName, bundle: nil)
+        self.tableview.register(titleDescriptionCell, forCellReuseIdentifier: self.noteDetailTitleDescriptionCellName)
+        
+    }
+    
+}
+
+// MARK: - UITableView Delegate
+
+extension NoteDetailViewController: UITableViewDelegate {}
+
+// MARK: - UITableView Data Source
+
+extension NoteDetailViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let titleDescriptionCell = self.tableview.dequeueReusableCell(withIdentifier: self.noteDetailTitleDescriptionCellName, for: indexPath) as! NoteDetailTitleDescriptionTableViewCell
+        return titleDescriptionCell
     }
     
 }
