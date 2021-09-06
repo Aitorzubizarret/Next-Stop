06aafdb0b77f0da7f2507f7dbc22db70b1f8e131
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2021-09-06 23:57:13 +0200

    Added a table view to the PlaceDetailViewController XIB file.

--- Next-Stop/Controllers/PlaceDetailViewController.swift
+++ Next-Stop/Controllers/PlaceDetailViewController.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 class PlaceDetailViewController: UIViewController {
-
+    
+    // MARK: - UI Elements
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Properties
+    
+    // MARK: - Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
