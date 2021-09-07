7c321811ea00b4c932b5ecf57b48770b63d962e8
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2021-09-07 22:38:57 +0200

    Updated PlaceDetailViewController to setup the table view.

--- Next-Stop/Controllers/PlaceDetailViewController.swift
+++ Next-Stop/Controllers/PlaceDetailViewController.swift
@@ -21,6 +21,7 @@ class PlaceDetailViewController: UIViewController {
         super.viewDidLoad()
         
         self.setupView()
+        self.setupTableView()
     }
 
     ///
@@ -28,6 +29,39 @@ class PlaceDetailViewController: UIViewController {
     ///
     private func setupView() {
         self.title = "Place Detail VC"
+    }
+    
+    ///
+    /// Setup the TableView.
+    ///
+    private func setupTableView() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        self.tableView.separatorStyle = .none
+    }
+    
+}
+
+// MARK: - UITableView Delegate
+
+extension PlaceDetailViewController: UITableViewDelegate {}
+
+// MARK: - UITableView Data Source
+
+extension PlaceDetailViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Cell"
+        return cell
     }
     
 }
