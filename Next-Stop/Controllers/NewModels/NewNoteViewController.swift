814296e5d041c520e674d725e52a1a2e6786d6d3
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2021-10-30 21:54:50 +0200

    Added a method to setup the NavBar (add a title to the NavBar for now) in NewTrip, NewPlace and NewNote view controllers.

--- Next-Stop/Controllers/NewModels/NewNoteViewController.swift
+++ Next-Stop/Controllers/NewModels/NewNoteViewController.swift
@@ -11,19 +11,15 @@ class NewNoteViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.setupNavbar()
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    ///
+    /// Setup the Navbar.
+    ///
+    private func setupNavbar() {
+        self.title = "New Note"
     }
-    */
-
+    
 }
